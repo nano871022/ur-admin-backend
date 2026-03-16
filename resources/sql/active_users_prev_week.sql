058d5916e0d1067596321956d14072c38d8124a2
Alejandro Parra <alejandroparral@gmail.com>
2026-03-16 08:25:32 -0500

    Update active_users_prev_week.sql

--- resources/sql/active_users_prev_week.sql
+++ resources/sql/active_users_prev_week.sql
@@ -1,7 +1,7 @@
 SELECT
-    COUNT(DISTINCT user_pseudo_id) as active_users,FORMAT_DATE("%Y%m%d",DATE_SUB(CURRENT_DATE(), INTERVAL 1 WEEK))  as ,FORMAT_DATE("%Y%m%d",CURRENT_DATE()) as
-
-    DATE_SUB(CURRENT_DATE(), INTERVAL 1 WEEK) as reference_date
+    COUNT(DISTINCT user_pseudo_id) as active_users,
+    FORMAT_DATE("%Y%m%d",DATE_SUB(CURRENT_DATE(), INTERVAL 1 WEEK))  as start_dt,
+    FORMAT_DATE("%Y%m%d",CURRENT_DATE()) as end_dt
 FROM
     `torressansebastian.analytics_437683494.events_*`
 WHERE
